20c5322745c2a40dac31703fb85b50f1cd81dcc2
Dkingofcode <doladepo128@gmail.com>
2023-04-01 19:41:31 +0100

    Fixed the react toastify display

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import ColorList from './ColorList';
 import Values from 'values.js';
 import { ToastContainer, toast } from 'react-toastify';
 
+
+
 function App() {
   const [colors, setColors] = useState(new Values('#f15025').all(10));
    toast.error('error');
@@ -15,7 +17,8 @@ function App() {
    const addColor = (color) => {
     try{
         const newColors = new Values(color).all(10);
-    }catch(error){ 
+         setColors(newColors)
+      }catch(error){ 
       toast.error(error.message);
     } 
     console.log(color);
--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { nanoid } from 'nanoid'; 
-
+import SingleColor from './SingleColor';
 
 const ColorList = ({ colors }) => {
   return (
